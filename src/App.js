cdeb3018884f17d6f95920066e1d03d3980ee57e
Kamya <Kamya@piktorlabs.com>
2021-01-05 00:00:01 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-
-
 import React from 'react';
 import './App.css';
 import { Header } from './components/header/Header';
 import { Main } from './components/main/Main';
 import { Footer } from './components/footer/Footer';
+import Carousel from 'react-elastic-carousel';
+import Card from './components/cards/card'
+
 
 export class App extends React.Component{
+
   render(){
+    let breakPoints = [
+      { width: 500 , itemsToShow : 1},
+      { width: 768 , itemsToShow : 2},
+      { width: 1200 , itemsToShow : 3},
+      { width: 1500 , itemsToShow : 4}
+    ];
     return(
       <div className="App">
         <Header/>
-        <Main />
+        <Carousel breakPoints={breakPoints}> 
+          <Card number="1"/>
+          <Card number="2"/>
+          <Card number="3"/>
+          <Card number="4"/>
+          <Card number="5"/>
+          <Card number="6"/>
+          <Card number="7"/>
+          <Card number="8"/>
+        </Carousel>
         <Footer />
       </div>
     );
@@ -20,20 +37,3 @@ export class App extends React.Component{
 
 
 
-
-
-// import './App.css';
-// import {Todos} from './components/Todos';
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Todos />   
-//     </div>
-//   );
-// }
-
-// export default App;
